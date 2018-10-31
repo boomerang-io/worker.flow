e35afd2fd03e6a39c575c23d99bd998f15cd901c
Marcus Roy <mdroy@us.ibm.com>
2018-10-31 15:42:32 +0200

    :rocket: download beta

--- commands/artifactory.js
+++ commands/artifactory.js
@@ -1,8 +1,35 @@
 const log = require("../log.js");
+const fetch = require("node-fetch");
+var fs = require('fs');
 
 module.exports = {
   downloadFile(req, inputProps) {
-    log.debug("Inside Artifactory Download File Plugin");
+
+    fetch('https://tools.boomerangplatform.net/artifactory/boomerang/test/hello', {
+      method: 'GET',
+      headers: {
+        'X-JFrog-Art-Api': 'AKCp5Z2NfDUZgvYrspbPwhR1byifksXAgJSFTssz5tG7wj41RyfgM1pJxPPn5FRZqTwrhtNZx'
+      }
+    })
+      .then(res => {
+        return new Promise((resolve, reject) => {
+          const dest = fs.createWriteStream('file');
+          res.body.pipe(dest);
+          fs.rename('file', '/data/file', function (err) {
+            if (err) throw err
+            console.log('Successfully renamed - AKA moved!')
+          })
+          res.body.on('error', err => {
+            reject(err);
+          });
+          dest.on('finish', () => {
+            resolve();
+          });
+          dest.on('error', err => {
+            reject(err);
+          });
+        });
+      });
   },
   uploadFile(req, inputProps) {
     log.debug("Inside Artifactory Upload File Plugin");
