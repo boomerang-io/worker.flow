c042a8b89e575f2c4f81d39f4a229911869ba194
TIMOTHY R. BULA <trbula@us.ibm.com>
2019-01-10 19:15:50 -0500

    feat: update getting props

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const inputOptions = {
 };
 
 const workflowProps = {
-  WF_PROPS_PATH: "/props",
+  WF_PROPS_PATH: "./props",
   WF_PROPS_PREFIX: "WF_PROPS_",
   WF_PROPS_PATTERN: /\$\{p:(.+)\}/
 };
@@ -17,4 +17,23 @@ const outputOptions = {
   unicode: true
 };
 
-module.exports = { inputOptions, workflowProps, outputOptions };
+const WORKFLOW_INPUT_PROPS_FILENAME = "workflow.input.properties";
+const WORKFLOW_SYSTEM_PROPS_FILENAME = "workflow.system.properties";
+const TASK_INPUT_PROPS_FILENAME = "task.input.properties";
+const TASK_SYSTEM_PROPS_FILENAME = "task.system.properties";
+
+const PROPS_FILES_CONFIG = {
+  WORKFLOW_INPUT_PROPS_FILENAME,
+  WORKFLOW_SYSTEM_PROPS_FILENAME,
+  TASK_INPUT_PROPS_FILENAME,
+  TASK_SYSTEM_PROPS_FILENAME,
+  PROPS_FILENAMES: [
+    WORKFLOW_INPUT_PROPS_FILENAME,
+    WORKFLOW_SYSTEM_PROPS_FILENAME,
+    TASK_INPUT_PROPS_FILENAME,
+    TASK_SYSTEM_PROPS_FILENAME
+  ],
+  INPUT_PROPS_FILENAME_PATTERN: /^.+\.output\.properties$/
+};
+
+module.exports = { inputOptions, workflowProps, outputOptions, PROPS_FILES_CONFIG };
