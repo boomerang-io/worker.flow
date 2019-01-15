6e5d70608e1f39173d9d9fd777325f6a68571b57
Isabela Mayumi Kawabata Borges <isabela.kawabata@ibm.com>
2019-01-15 10:53:42 -0200

    feat(script): local Added local script

--- config.js
+++ config.js
@@ -5,9 +5,8 @@ const inputOptions = {
   variables: false,
   include: false
 };
-
 const workflowProps = {
-  WF_PROPS_PATH: "/props",
+  WF_PROPS_PATH: process.env.NODE_ENV === "local"? "./props" : "/props",
   WF_PROPS_PREFIX: "WF_PROPS_",
   WF_PROPS_PATTERN: /\$\{p:(.+)\}/
 };
