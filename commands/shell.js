9cfcc750d5c5acc872bdf5cd0e25a0307d86e371
Tyson Lawrie <twlawrie@us.ibm.com>
2019-01-22 20:13:07 +1100

    :bug: PLT-2109 not handling multiple property references in the one input property

--- commands/shell.js
+++ commands/shell.js
@@ -39,6 +39,8 @@ module.exports = {
       }
     });
 
+    utils.setOutputProperty("this", "that");
+
     log.debug("End Shell Plugin");
   }
 };
--- config.js
+++ config.js
@@ -10,7 +10,8 @@ const inputOptions = {
 const workflowProps = {
   WF_PROPS_PATH: NODE_ENV === "local" ? "./props" : "/props",
   //WF_PROPS_PREFIX: "WF_PROPS_", //no longer used
-  WF_PROPS_PATTERN: /\$\{p:(.+)\}/
+  //old pattern: /\$\{p:(.+)\}/
+  WF_PROPS_PATTERN: /\$\{p:([\w\ \.]*\/?[\w\.]+)\}/g
 };
 
 const outputOptions = {
