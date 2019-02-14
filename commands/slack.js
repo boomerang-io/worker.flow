0cac78e07c1c21ca06662baeb56e97dee6648695
Timothy R. Bula <trbula@us.ibm.com>
2019-02-13 20:11:45 -0500

    chore: more cleanup

--- commands/slack.js
+++ commands/slack.js
@@ -17,7 +17,7 @@ module.exports = {
       "***REMOVED***";
     let webhook = new IncomingWebhook(url);
 
-    //TODO: see if we can set the proxy at the higher CLI level rather than have each plugin have to support a proxy
+    /** @todo see if we can set the proxy at the higher CLI level rather than have each plugin have to support a proxy*/
     if (process.env.HTTP_PROXY) {
       log.debug("Using Proxy", process.env.HTTP_PROXY);
       webhook = new IncomingWebhook(url, {
@@ -25,7 +25,7 @@ module.exports = {
       });
     }
 
-    //TODO: Implement a variable check
+    /** @todo Implement a variable check */
     if (!channel) {
       log.debug(channel);
       log.err("Channel or user has not been set");
@@ -52,7 +52,7 @@ module.exports = {
       },
       async function(err, res) {
         if (err) {
-          //TODO: Catch HTTP error for timeout so we can return better exits
+          /** @todo Catch HTTP error for timeout so we can return better exits */
           log.err("Slack sendWebhook error", err);
           return process.exit(1);
         } else {
