abb336fc131cb34cf9083fd48794b9badd3b8084
Tyson Lawrie <twlawrie@us.ibm.com>
2019-06-06 16:30:19 +1000

    :rocket: further work on the framework and progressing java and lib jar modes

--- commands/build.js
+++ commands/build.js
@@ -35,16 +35,12 @@ module.exports = {
       log.ci("Retrieving Source Code");
       await exec(shellDir + '/common/git-clone.sh ' + taskProps['component/repoSshUrl'] + ' ' + taskProps['component/repoUrl'] + ' ' + taskProps['git.commit.id']);
       shell.cd("/data/workspace");
-      log.ci("Compile Artifact(s)");
       if (taskProps['system.mode'] === "java.lib") {
-        await exec(shellDir + '/build/compile-jar.sh ' + taskProps['build.tool'] + ' ' + taskProps['build.tool.version'] + ' ' + taskProps['version.name']);
+        log.ci("Compile & Package Artifact(s)");
+        await exec(shellDir + '/build/compile-package-jar.sh ' + taskProps['build.tool'] + ' ' + taskProps['build.tool.version'] + ' ' + taskProps['version.name'] + ' ' + JSON.stringify(taskProps['global/maven.repo.url']) + ' ' + taskProps['global/maven.repo.id'] + ' ' + taskProps['global/artifactory.user'] + ' ' + taskProps['global/artifactory.password']);
       } else {
+        log.ci("Compile Artifact(s)");
         await exec(shellDir + '/build/compile.sh ' + taskProps['build.tool'] + ' ' + taskProps['build.tool.version'] + ' ' + taskProps['version.name']);
-      }
-      log.ci("Package");
-      if (taskProps['system.mode'] === "java.lib") {
-        log.debug("Packaging for Maven repository")
-        await exec(shellDir + '/build/package-maven.sh ' + JSON.stringify(taskProps['global/maven.repo.url']) + ' ' + taskProps['global/maven.repo.id'] + ' ' + taskProps['global/artifactory.user'] + ' ' + taskProps['global/artifactory.password']);
       }
       if (taskProps['docker.enable']) {
         log.debug("Packaging for Docker registry")
