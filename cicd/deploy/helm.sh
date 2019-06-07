9fdffedf831d51346f39bfd07f1e4dbc688fbdcc
Tyson Lawrie <twlawrie@us.ibm.com>
2019-06-07 15:47:07 +1000

    Fixed merge issues

--- cicd/deploy/helm.sh
+++ cicd/deploy/helm.sh
@@ -4,11 +4,11 @@ HELM_REPO_URL=$1
 CHART_NAME=$2
 CHART_RELEASE=$3
 HELM_IMAGE_TAG=$4
+VERSION_NAME=$5
 
-helm --home $HELM_RESOURCE_PATH repo add boomerang-charts ${arr['global/helm.repo.url']}
+helm --home $HELM_RESOURCE_PATH repo add boomerang-charts $HELM_REPO_URL
 
 CHART_VERSION=`helm list --home $HELM_RESOURCE_PATH $HELM_TLS_STRING --debug --kube-context $KUBE_CLUSTER_HOST-context $CHART_RELEASE | grep $CHART_NAME | rev | cut -d $'\t' -f 2- | sed -e 's/^[[:space:]]*//' | cut -d ' ' -f 1 | rev | sed 's/.*-//'`
 echo "Chart Version: $CHART_VERSION"
 
-VERSION_NAME=${arr['version.name']}
 helm upgrade --home $HELM_RESOURCE_PATH --debug --set $HELM_IMAGE_TAG=$VERSION_NAME --kube-context $KUBE_CLUSTER_HOST-context $HELM_TLS_STRING --reuse-values --version $CHART_VERSION $CHART_RELEASE boomerang-charts/$CHART_NAME
--- commands/deploy.js
+++ commands/deploy.js
@@ -4,13 +4,13 @@ const { CICDError } = require('../error.js')
 const shell = require("shelljs");
 const fileCommand = require("./file.js");
 
-tion exec(command) {
-turn new Promise(function (resolve, reject) {
-  g.debug("Command directory:", shell.pwd().toString());
-    debug("Command to execute:", command);
-  ell.exec(command, config, function (code, stdout, stderr) {
-  if (code) {
-    reject(new CICDError(code, stderr));
+function exec(command) {
+  return new Promise(function (resolve, reject) {
+    log.debug("Command directory:", shell.pwd().toString());
+    log.debug("Command to execute:", command);
+    shell.exec(command, config, function (code, stdout, stderr) {
+      if (code) {
+        reject(new CICDError(code, stderr));
       }
       resolve(stdout ? stdout : stderr);
     });
@@ -21,9 +21,7 @@ module.exports = {
   async execute() {
     log.debug("Inside Deploy Plugin");
 
-    //Destructure and get properties ready.
     const taskProps = utils.resolveCICDTaskInputProps();
-    // const { path, script } = taskProps;
     const shellDir = "/cli/cicd";
     config = {
       verbose: true,
@@ -32,7 +30,6 @@ module.exports = {
     try {
       shell.cd("/data");
       log.ci("Initializing Dependencies");
-<<<<<<< HEAD
       await exec(shellDir + '/deploy/initialize-dependencies.sh ' + taskProps['deploy.type'] + ' ' + taskProps['deploy.kube.version']);
       if (taskProps['deploy.type'] === "kubernetes") {
         var kubePort = "8080";
@@ -42,12 +39,9 @@ module.exports = {
         fileCommand.replaceTokensInFileWithProps(shellDir + '/deploy', 'kube.yaml', '@', '@', taskProps, undefined);
         await exec(shellDir + '/deploy/kubernetes.sh ' + shellDir + '/deploy/kube.yaml');
       } else if (taskProps['deploy.type'] === "helm") {
-        await exec(shellDir + '/deploy/helm.sh ' + taskProps['global/helm.repo.url'] + ' ' + taskProps['global/deploy.helm.chart'] + ' ' + taskProps['global/deploy.helm.release'] + ' ' + taskProps['global/helm.image.tag']);
+        await exec(shellDir + '/deploy/helm.sh ' + taskProps['global/helm.repo.url'] + ' ' + taskProps['global/deploy.helm.chart'] + ' ' + taskProps['global/deploy.helm.release'] + ' ' + taskProps['global/helm.image.tag'] + ' ' + taskProps['version.name']);
       }
-=======
-      await exec(shellDir + '/deploy/initialize-dependencies.sh ' + taskProps['build.tool'] + ' ' + taskProps['build.tool.version']);
->>>>>>> c06bd099aba27ba3a4698468827cf8d4315e09ec
-      await exec(shellDir + '/common/footer.sh');
+      await exec(shell Dir + '/common/footer.sh');
     } catch (e) {
       log.err("  Error encountered. Code: " + e.code + ", Message:", e.message);
       process.exit(1);
