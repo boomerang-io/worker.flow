6b5465a5d3f943eebc50750092378810e5d7e366
Glen Hickman <gchickma@us.ibm.com>
2019-07-30 15:31:40 -0500

    add package python wheel

--- cicd/build/compile-package-python-wheel.sh
+++ cicd/build/compile-package-python-wheel.sh
@@ -3,10 +3,11 @@
 # ( echo "\n"; echo "%.0s-" {1..30}; echo " Build Artifact "; echo "%.0s-" {1..30}; echo "\n\n" )
 
 BUILD_LANGUAGE_VERSION=$1
-ART_URL=$2
-ART_REPO_ID=$3
-ART_REPO_USER=$4
-ART_REPO_PASSWORD=$5
+VERSION_NAME=$2
+ART_URL=$3
+ART_REPO_ID=$4
+ART_REPO_USER=$5
+ART_REPO_PASSWORD=$6
 ART_REPO_HOME=~/.pypirc
 
 cat >> $ART_REPO_HOME <<EOL
@@ -17,6 +18,8 @@ repository: $ART_URL/$ART_REPO_ID
 username: $ART_REPO_USER
 password: $ART_REPO_PASSWORD
 EOL
+
+# TODO Determine if we need to override `version` in setup.py using $VERSION_NAME
 
 if [ "$BUILD_LANGUAGE_VERSION" == "2" ]; then
 	python -m pip install --user --upgrade setuptools wheel
