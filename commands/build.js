48a7f20d289d5ba20f883ca349c8da9ca5855834
Tyson Lawrie <twlawrie@us.ibm.com>
2019-07-31 16:48:54 -0500

    Further work

--- commands/build.js
+++ commands/build.js
@@ -29,58 +29,18 @@ module.exports = {
     };
 
     try {
-      await exec(
-        shellDir + "/common/initialize.sh " + taskProps["language.version"]
-      );
+      await exec(shellDir + "/common/initialize.sh " + taskProps["language.version"]);
       log.ci("Initializing Dependencies");
-      if (
-        taskProps["system.mode"] === "lib.jar" ||
-        taskProps["system.mode"] === "java"
-      ) {
-        await exec(
-          shellDir +
-            "/common/initialize-dependencies-java.sh " +
-            taskProps["language.version"]
-        );
-        await exec(
-          shellDir +
-            "/common/initialize-dependencies-java-tool.sh " +
-            taskProps["build.tool"] +
-            " " +
-            taskProps["build.tool.version"]
-        );
+      if (taskProps["system.mode"] === "lib.jar" || taskProps["system.mode"] === "java") {
+        await exec(shellDir + "/common/initialize-dependencies-java.sh " + taskProps["language.version"]);
+        await exec(shellDir + "/common/initialize-dependencies-java-tool.sh " + taskProps["build.tool"] + " " + taskProps["build.tool.version"]);
       } else if (taskProps["system.mode"] === "nodejs") {
-        await exec(
-          shellDir +
-            "/common/initialize-dependencies-node.sh " +
-            taskProps["build.tool"] +
-            " " +
-            JSON.stringify(taskProps["global/artifactory.url"]) +
-            " " +
-            taskProps["global/artifactory.user"] +
-            " " +
-            taskProps["global/artifactory.password"]
-        );
-      } else if (
-        taskProps["system.mode"] === "python" ||
-        taskProps["system.mode"] === "lib.wheel"
-      ) {
-        await exec(
-          shellDir +
-            "/common/initialize-dependencies-python.sh " +
-            taskProps["language.version"]
-        );
+        await exec(shellDir + "/common/initialize-dependencies-node.sh " + taskProps["build.tool"] + " " + JSON.stringify(taskProps["global/artifactory.url"]) + " " + taskProps["global/artifactory.user"] + " " + taskProps["global/artifactory.password"]);
+      } else if (taskProps["system.mode"] === "python" || taskProps["system.mode"] === "lib.wheel") {
+        await exec(shellDir + "/common/initialize-dependencies-python.sh " + taskProps["language.version"]);
       }
       log.ci("Retrieving Source Code");
-      await exec(
-        shellDir +
-          "/common/git-clone.sh " +
-          taskProps["component/repoSshUrl"] +
-          " " +
-          taskProps["component/repoUrl"] +
-          " " +
-          taskProps["git.commit.id"]
-      );
+      await exec(shellDir + "/common/git-clone.sh " + taskProps["component/repoSshUrl"] + " " + taskProps["component/repoUrl"] + " " + taskProps["git.commit.id"]);
       shell.cd("/data/workspace");
       if (taskProps["system.mode"] === "lib.jar") {
         log.ci("Compile & Package Artifact(s)");
@@ -91,10 +51,7 @@ module.exports = {
             " " +
             taskProps["build.tool.version"] +
             " " +
-            taskProps["version.name"].substr(
-              0,
-              taskProps["version.name"].lastIndexOf("-")
-            ) +
+            taskProps["version.name"].substr(0, taskProps["version.name"].lastIndexOf("-")) +
             " " +
             JSON.stringify(taskProps["global/maven.repo.url"]) +
             " " +
@@ -125,14 +82,10 @@ module.exports = {
         );
       } else if (taskProps["system.mode"] === "nodejs") {
         log.ci("Compile Artifact(s)");
-        await exec(
-          shellDir + "/build/compile-node.sh " + taskProps["build.tool"]
-        );
+        await exec(shellDir + "/build/compile-node.sh " + taskProps["build.tool"]);
       } else if (taskProps["system.mode"] === "python") {
         log.ci("Compile Artifact(s)");
-        await exec(
-          shellDir + "/build/compile-python.sh " + taskProps["language.version"]
-        );
+        await exec(shellDir + "/build/compile-python.sh " + taskProps["language.version"]);
       } else if (taskProps["system.mode"] === "lib.wheel") {
         log.ci("Compile & Package Artifact(s)");
         await exec(
@@ -140,10 +93,7 @@ module.exports = {
             "/build/compile-package-python-wheel.sh " +
             taskProps["language.version"] +
             " " +
-            taskProps["version.name"].substr(
-              0,
-              taskProps["version.name"].lastIndexOf("-")
-            ) +
+            taskProps["version.name"].substr(0, taskProps["version.name"].lastIndexOf("-")) +
             " " +
             JSON.stringify(taskProps["global/pypi.repo.url"]) +
             " " +
@@ -154,6 +104,7 @@ module.exports = {
             taskProps["global/pypi.repo.password"]
         );
       }
+      await exec("ls -ltR");
       if (taskProps["docker.enable"]) {
         log.ci("Packaging for Docker registry");
         await exec(
@@ -186,93 +137,6 @@ module.exports = {
     } finally {
       await exec(shellDir + "/common/footer.sh");
       log.debug("Finished CICD Build Activity");
-    }
-  },
-  async lib_jar() {
-    log.debug("Started CICD Lib Jar Build Activity");
-
-    //Destructure and get properties ready.
-    const taskProps = utils.resolveCICDTaskInputProps();
-    // const { path, script } = taskProps;
-    const shellDir = "/cli/cicd";
-    config = {
-      verbose: true
-    };
-
-    try {
-      shell.cd("/data");
-      log.ci("Initializing Dependencies");
-      await exec(
-        shellDir +
-          "/common/initialize-dependencies-java.sh " +
-          taskProps["build.tool"] +
-          " " +
-          taskProps["build.tool.version"] +
-          " " +
-          JSON.stringify(taskProps["global/artifactory.url"]) +
-          " " +
-          taskProps["global/artifactory.user"] +
-          " " +
-          taskProps["global/artifactory.password"]
-      );
-      log.ci("Retrieving Source Code");
-      await exec(
-        shellDir +
-          "/common/git-clone.sh " +
-          taskProps["component/repoSshUrl"] +
-          " " +
-          taskProps["component/repoUrl"] +
-          " " +
-          taskProps["git.commit.id"]
-      );
-      shell.cd("/data/workspace");
-      log.ci("Compile & Package Artifact(s)");
-      await exec(
-        shellDir +
-          "/build/compile-package-jar.sh " +
-          taskProps["build.tool"] +
-          " " +
-          taskProps["build.tool.version"] +
-          " " +
-          taskProps["version.name"].substring(
-            0,
-            taskProps["version.name"].lastIndexOf("-")
-          ) +
-          " " +
-          JSON.stringify(taskProps["global/maven.repo.url"]) +
-          " " +
-          taskProps["global/maven.repo.id"] +
-          " " +
-          taskProps["global/artifactory.user"] +
-          " " +
-          taskProps["global/artifactory.password"]
-      );
-      if (taskProps["docker.enable"]) {
-        log.ci("Packaging for Docker registry");
-        await exec(
-          shellDir +
-            "/build/package-docker.sh " +
-            taskProps["docker.image.name"] +
-            " " +
-            taskProps["version.name"] +
-            " " +
-            JSON.stringify(taskProps["team.name"]) +
-            " " +
-            JSON.stringify(taskProps["global/container.registry.host"]) +
-            " " +
-            taskProps["global/container.registry.port"] +
-            " " +
-            taskProps["global/container.registry.user"] +
-            " " +
-            taskProps["global/container.registry.password"]
-        );
-      }
-      await exec(shellDir + "/common/footer.sh");
-    } catch (e) {
-      log.err("  Error encountered. Code: " + e.code + ", Message:", e.message);
-      process.exit(1);
-    } finally {
-      log.debug("Finished CICD Lib Jar Build Activity");
     }
   }
 };
