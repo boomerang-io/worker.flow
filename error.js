08400df08ce5b549e7c3a945be3769ec71f16526
Tyson Lawrie <twlawrie@us.ibm.com>
2019-08-01 14:24:35 -0500

    Further work for the 5.2 release

--- error.js
+++ error.js
@@ -4,44 +4,50 @@ class CICDError extends Error {
   constructor(code, stack) {
     var definedMessage = "An unknown error occurred. Please speak to your support representative.";
     switch (code) {
+      case 90:
+        definedMessage = "An error occurred during the build of the Dockerfile. Please review the error messsage or speak to your support representative.";
+        break;
+      case 91:
+        definedMessage = "Helm chart upgrade failed. Please review the error message.";
+        break;
       case 92:
-        definedMessage = "Helm chart release is required to auto detect the helm chart. To override specify the helm chart(s) in your CI stage as a comma delimited list."
+        definedMessage = "Helm chart release is required to auto detect the helm chart. To override specify the helm chart(s) in your CI stage as a comma delimited list.";
         break;
       case 93:
-        definedMessage = "Kubernetes namespace is required to auto detect helm release. To override specify helm release in your CI stage."
+        definedMessage = "Kubernetes namespace is required to auto detect helm release. To override specify helm release in your CI stage.";
         break;
       case 94:
-        definedMessage = "Unable to detect helm chart version."
+        definedMessage = "Unable to detect helm chart version.";
         break;
       case 95:
-        definedMessage = "Unable to find test command."
+        definedMessage = "Unable to find test command.";
         break;
       case 96:
-        definedMessage = "Unable to find Dockerfile."
+        definedMessage = "Unable to find Dockerfile.";
         break;
       case 97:
-        definedMessage = "Unable to find build command."
+        definedMessage = "Unable to find build command.";
         break;
       case 98:
-        definedMessage = "Unknown property encountered."
+        definedMessage = "Unknown property encountered.";
         break;
       case 99:
-        definedMessage = "No build tool and/or language specified but required."
+        definedMessage = "No build tool and/or language specified but required.";
         break;
       case 127:
-        definedMessage = "A file required was not found."
+        definedMessage = "A file required was not found.";
         break;
       default:
         break;
     }
-    var fullMessage = definedMessage + '\n' + stack;
-    super(fullMessage)
-    this.message = fullMessage
-    this.name = 'CICDError';
+    var fullMessage = definedMessage + "\n" + stack;
+    super(fullMessage);
+    this.message = fullMessage;
+    this.name = "CICDError";
     this.code = code;
   }
 }
 
 module.exports = {
   CICDError
-}
+};
