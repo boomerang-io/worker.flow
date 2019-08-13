6f3691ebc8674e9e77cb931d88a0ad50818f0781
Tyson Lawrie <twlawrie@us.ibm.com>
2019-08-13 18:54:05 -0400

    :rocket: added glibc java unit test dependency

--- commands/test.js
+++ commands/test.js
@@ -1,15 +1,15 @@
 const log = require("../log.js");
 const utils = require("../utils.js");
-const { CICDError } = require('../error.js')
+const { CICDError } = require("../error.js");
 const shell = require("shelljs");
 const fs = require("fs");
 const fileCommand = require("./file.js");
 
 function exec(command) {
-  return new Promise(function (resolve, reject) {
+  return new Promise(function(resolve, reject) {
     log.debug("Command directory:", shell.pwd().toString());
     log.debug("Command to execute:", command);
-    shell.exec(command, config, function (code, stdout, stderr) {
+    shell.exec(command, config, function(code, stdout, stderr) {
       if (code) {
         reject(new CICDError(code, stderr));
       }
@@ -25,10 +25,10 @@ module.exports = {
     const taskProps = utils.resolveCICDTaskInputProps();
     const shellDir = "/cli/cicd";
     config = {
-      verbose: true,
-    }
+      verbose: true
+    };
 
-    var testTypes = taskProps['test.type'] !== undefined && taskProps['test.type'] !== null ? taskProps['test.type'].split(',') : [];
+    var testTypes = taskProps["test.type"] !== undefined && taskProps["test.type"] !== null ? taskProps["test.type"].split(",") : [];
 
     try {
       if (!testTypes.length) {
@@ -36,53 +36,54 @@ module.exports = {
       } else {
         shell.cd("/data");
         log.ci("Initializing Dependencies");
-        if (taskProps['system.mode'] === "lib.jar" || taskProps['system.mode'] === "java") {
-          await exec(shellDir + '/common/initialize-dependencies-java.sh ' + taskProps['language.version']);
+        if (taskProps["system.mode"] === "lib.jar" || taskProps["system.mode"] === "java") {
+          await exec(shellDir + "/common/initialize-dependencies-java.sh " + taskProps["language.version"]);
           log.ci("Initializing Language Dependencies");
-          await exec(shellDir + '/common/initialize-dependencies-java-tool.sh ' + taskProps['build.tool'] + ' ' + taskProps['build.tool.version']);
-        } else if (taskProps['system.mode'] === "nodejs") {
-          await exec(shellDir + '/common/initialize-dependencies-node.sh ' + taskProps['build.tool'] + ' ' + JSON.stringify(taskProps['global/artifactory.url']) + ' ' + taskProps['global/artifactory.user'] + ' ' + taskProps['global/artifactory.password']);
+          await exec(shellDir + "/common/initialize-dependencies-java-tool.sh " + taskProps["build.tool"] + " " + taskProps["build.tool.version"]);
+        } else if (taskProps["system.mode"] === "nodejs") {
+          await exec(shellDir + "/common/initialize-dependencies-node.sh " + taskProps["build.tool"] + " " + JSON.stringify(taskProps["global/artifactory.url"]) + " " + taskProps["global/artifactory.user"] + " " + taskProps["global/artifactory.password"]);
         }
         log.ci("Retrieving Source Code");
-        await exec(shellDir + '/common/git-clone.sh ' + taskProps['component/repoSshUrl'] + ' ' + taskProps['component/repoUrl'] + ' ' + taskProps['git.commit.id']);
+        await exec(shellDir + "/common/git-clone.sh " + taskProps["component/repoSshUrl"] + " " + taskProps["component/repoUrl"] + " " + taskProps["git.commit.id"]);
         shell.cd("/data/workspace");
-        if (taskProps['system.mode'] === "lib.jar" || taskProps['system.mode'] === "java") {
+        if (taskProps["system.mode"] === "lib.jar" || taskProps["system.mode"] === "java") {
           if (!fileCommand.checkFileContainsStringWithProps("/data/workspace/pom.xml", "<plugins>", undefined, false)) {
             log.debug("No Maven plugins found, adding...");
-            var replacementString = fs.readFileSync(shellDir + '/test/unit-java-maven-plugins.xml', "utf-8");
+            var replacementString = fs.readFileSync(shellDir + "/test/unit-java-maven-plugins.xml", "utf-8");
             fileCommand.replaceStringInFileWithProps("/data/workspace/pom.xml", "<plugins>", replacementString, undefined, false);
           }
           if (!fileCommand.checkFileContainsStringWithProps("/data/workspace/pom.xml", "<artifactId>jacoco-maven-plugin</artifactId>", undefined, false)) {
             log.debug("...adding jacoco-maven-plugin.");
-            var replacementString = fs.readFileSync(shellDir + '/test/unit-java-maven-jacoco.xml', "utf-8");
+            var replacementString = fs.readFileSync(shellDir + "/test/unit-java-maven-jacoco.xml", "utf-8");
             fileCommand.replaceStringInFileWithProps("/data/workspace/pom.xml", "<plugins>", replacementString, undefined, false);
           }
           if (!fileCommand.checkFileContainsStringWithProps("/data/workspace/pom.xml", "<artifactId>sonar-maven-plugin</artifactId>", undefined, false)) {
             log.debug("...adding sonar-maven-plugin.");
-            var replacementString = fs.readFileSync(shellDir + '/test/unit-java-maven-sonar.xml', "utf-8");
+            var replacementString = fs.readFileSync(shellDir + "/test/unit-java-maven-sonar.xml", "utf-8");
             fileCommand.replaceStringInFileWithProps("/data/workspace/pom.xml", "<plugins>", replacementString, undefined, false);
           }
           if (!fileCommand.checkFileContainsStringWithProps("/data/workspace/pom.xml", "<artifactId>maven-surefire-report-plugin</artifactId>", undefined, false)) {
             log.debug("...adding maven-surefire-report-plugin.");
-            var replacementString = fs.readFileSync(shellDir + '/test/unit-java-maven-surefire.xml', "utf-8");
+            var replacementString = fs.readFileSync(shellDir + "/test/unit-java-maven-surefire.xml", "utf-8");
             fileCommand.replaceStringInFileWithProps("/data/workspace/pom.xml", "<plugins>", replacementString, undefined, false);
           }
           if (testTypes.includes("static")) {
             log.debug("Commencing static tests");
-            await exec(shellDir + '/test/static-java.sh ' + taskProps['build.tool'] + ' ' + taskProps['version.name'] + ' ' + taskProps['global/sonar.url'] + ' ' + taskProps['global/sonar.api.key'] + ' ' + taskProps['system.component.id'] + ' ' + taskProps['system.component.name']);
+            await exec(shellDir + "/test/static-java.sh " + taskProps["build.tool"] + " " + taskProps["version.name"] + " " + taskProps["global/sonar.url"] + " " + taskProps["global/sonar.api.key"] + " " + taskProps["system.component.id"] + " " + taskProps["system.component.name"]);
           }
           if (testTypes.includes("unit")) {
             log.debug("Commencing unit tests");
-            await exec(shellDir + '/test/unit-java.sh ' + taskProps['build.tool'] + ' ' + taskProps['version.name'] + ' ' + taskProps['global/sonar.url'] + ' ' + taskProps['global/sonar.api.key'] + ' ' + taskProps['system.component.id'] + ' ' + taskProps['system.component.name']);
+            await exec(shellDir + "/test/initialize-dependencies-unit-java.sh");
+            await exec(shellDir + "/test/unit-java.sh " + taskProps["build.tool"] + " " + taskProps["version.name"] + " " + taskProps["global/sonar.url"] + " " + taskProps["global/sonar.api.key"] + " " + taskProps["system.component.id"] + " " + taskProps["system.component.name"]);
           }
-        } else if (taskProps['system.mode'] === "nodejs") {
+        } else if (taskProps["system.mode"] === "nodejs") {
           if (testTypes.includes("static")) {
             log.debug("Commencing static tests");
-            await exec(shellDir + '/test/static-node.sh ' + taskProps['build.tool'] + ' ' + taskProps['version.name'] + ' ' + taskProps['global/sonar.url'] + ' ' + taskProps['global/sonar.api.key'] + ' ' + taskProps['system.component.id'] + ' ' + taskProps['system.component.name']);
+            await exec(shellDir + "/test/static-node.sh " + taskProps["build.tool"] + " " + taskProps["version.name"] + " " + taskProps["global/sonar.url"] + " " + taskProps["global/sonar.api.key"] + " " + taskProps["system.component.id"] + " " + taskProps["system.component.name"]);
           }
           if (testTypes.includes("unit")) {
             log.debug("Commencing static tests");
-            await exec(shellDir + '/test/unit-node.sh ' + taskProps['build.tool'] + ' ' + taskProps['version.name'] + ' ' + taskProps['global/sonar.url'] + ' ' + taskProps['global/sonar.api.key'] + ' ' + taskProps['system.component.id'] + ' ' + taskProps['system.component.name']);
+            await exec(shellDir + "/test/unit-node.sh " + taskProps["build.tool"] + " " + taskProps["version.name"] + " " + taskProps["global/sonar.url"] + " " + taskProps["global/sonar.api.key"] + " " + taskProps["system.component.id"] + " " + taskProps["system.component.name"]);
           }
         }
       }
@@ -90,7 +91,7 @@ module.exports = {
       log.err("  Error encountered. Code: " + e.code + ", Message:", e.message);
       process.exit(1);
     } finally {
-      await exec(shellDir + '/common/footer.sh');
+      await exec(shellDir + "/common/footer.sh");
       log.debug("Finished CICD Test Activity");
     }
   }
