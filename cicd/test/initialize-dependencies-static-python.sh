60860f510740e32d75288e475768b33df6432124
Glen Hickman <gchickma@us.ibm.com>
2019-11-20 21:21:23 -0600

    install pylint via pip/pip3

--- cicd/test/initialize-dependencies-static-python.sh
+++ cicd/test/initialize-dependencies-static-python.sh
@@ -2,6 +2,14 @@
 
 # ( printf '\n'; printf '%.0s-' {1..30}; printf ' Initialize Python Static Test Dependencies '; printf '%.0s-' {1..30}; printf '\n\n' )
 
-apk add pylint
+BUILD_LANGUAGE_VERSION=$1
+
+if [ "$BUILD_LANGUAGE_VERSION" == "2" ]; then
+    pip install pylint
+elif [ "$BUILD_LANGUAGE_VERSION" == "3" ]; then
+    pip3 install pylint
+else
+	exit 99
+fi
 
 pylint --version
