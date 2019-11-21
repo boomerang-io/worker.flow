15b6063e1f7dd4c71bf5e63ba61ccc3d013f6374
Glen Hickman <gchickma@us.ibm.com>
2019-11-20 21:09:29 -0600

    add python logging

--- cicd/test/initialize-dependencies-static-python.sh
+++ cicd/test/initialize-dependencies-static-python.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# ( printf '\n'; printf '%.0s-' {1..30}; printf ' Initialize Python Static Test Dependencies '; printf '%.0s-' {1..30}; printf '\n\n' )
+
 apk add pylint
+
+pylint --version
