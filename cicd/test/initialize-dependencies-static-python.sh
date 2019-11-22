8daff8093aee0a152597a3aa01e2e60d1e52ed44
Glen Hickman <gchickma@us.ibm.com>
2019-11-22 09:23:31 -0600

    add .pip.conf for python sca

--- cicd/test/initialize-dependencies-static-python.sh
+++ cicd/test/initialize-dependencies-static-python.sh
@@ -3,6 +3,22 @@
 # ( printf '\n'; printf '%.0s-' {1..30}; printf ' Initialize Python Static Test Dependencies '; printf '%.0s-' {1..30}; printf '\n\n' )
 
 BUILD_LANGUAGE_VERSION=$1
+ART_REGISTRY_HOST=$2
+ART_REPO_ID=$3
+ART_REPO_USER=$4
+ART_REPO_PASSWORD=$5
+
+# Create Artifactory references for library download
+PIP_CONF=~/.pip.conf
+cat >> $PIP_CONF <<EOL
+[global]
+extra-index-url=https://$ART_REPO_USER:$ART_REPO_PASSWORD@$ART_REGISTRY_HOST/artifactory/api/pypi/$ART_REPO_ID/simple
+[install]
+extra-index-url=https://$ART_REPO_USER:$ART_REPO_PASSWORD@$ART_REGISTRY_HOST/artifactory/api/pypi/$ART_REPO_ID/simple
+EOL
+
+# Export pip config home
+export PIP_CONFIG_FILE=$PIP_CONF
 
 if [ "$BUILD_LANGUAGE_VERSION" == "2" ]; then
 	pip install --upgrade pip
