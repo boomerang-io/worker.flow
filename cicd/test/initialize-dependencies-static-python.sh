2795d2ea473b437b30c0ede98d12342528eefcdd
Glen Hickman <gchickma@us.ibm.com>
2019-11-22 08:52:01 -0600

    install from requirements.txt if exists for python sca

--- cicd/test/initialize-dependencies-static-python.sh
+++ cicd/test/initialize-dependencies-static-python.sh
@@ -5,9 +5,35 @@
 BUILD_LANGUAGE_VERSION=$1
 
 if [ "$BUILD_LANGUAGE_VERSION" == "2" ]; then
-    pip install flask requests pylint nose coverage nosexcover
+	pip install --upgrade pip
+	RESULT=$?
+	if [ $RESULT -ne 0 ] ; then
+		exit 89
+	fi
+	if [ -f requirements.txt ]; then
+	    echo "Using requirements.txt file found in project to install dependencies"
+	    pip install -r requirements.txt
+		RESULT=$?
+		if [ $RESULT -ne 0 ] ; then
+			exit 89
+		fi
+	fi
+  pip install pylint nose coverage nosexcover
 elif [ "$BUILD_LANGUAGE_VERSION" == "3" ]; then
-    pip3 install flask requests pylint nose coverage nosexcover
+  pip3 install --upgrade pip
+	RESULT=$?
+	if [ $RESULT -ne 0 ] ; then
+		exit 89
+	fi
+	if [ -f requirements.txt ]; then
+	    echo "Using requirements.txt file found in project to install dependencies"
+	    pip3 install -r requirements.txt
+		RESULT=$?
+		if [ $RESULT -ne 0 ] ; then
+			exit 89
+		fi
+	fi
+  pip3 install pylint nose coverage nosexcover
 else
 	exit 99
 fi
