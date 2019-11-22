b9a26cecea55548b4cc514b44ea35c50e556d0a9
Glen Hickman <gchickma@us.ibm.com>
2019-11-22 08:14:44 -0600

    add nosexcover to python deps

--- cicd/test/initialize-dependencies-static-python.sh
+++ cicd/test/initialize-dependencies-static-python.sh
@@ -5,9 +5,9 @@
 BUILD_LANGUAGE_VERSION=$1
 
 if [ "$BUILD_LANGUAGE_VERSION" == "2" ]; then
-    pip install pylint nose coverage
+    pip install pylint nose coverage nosexcover
 elif [ "$BUILD_LANGUAGE_VERSION" == "3" ]; then
-    pip3 install pylint nose coverage
+    pip3 install pylint nose coverage nosexcover
 else
 	exit 99
 fi
