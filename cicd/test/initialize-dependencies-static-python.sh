4d78940193046b7b09a8f0a168f3ba6d981e742f
Glen Hickman <gchickma@us.ibm.com>
2019-11-21 18:54:03 -0600

    remove distribute

--- cicd/test/initialize-dependencies-static-python.sh
+++ cicd/test/initialize-dependencies-static-python.sh
@@ -5,9 +5,9 @@
 BUILD_LANGUAGE_VERSION=$1
 
 if [ "$BUILD_LANGUAGE_VERSION" == "2" ]; then
-    pip install pylint nose coverage distribute
+    pip install pylint nose coverage
 elif [ "$BUILD_LANGUAGE_VERSION" == "3" ]; then
-    pip3 install pylint nose coverage distribute
+    pip3 install pylint nose coverage
 else
 	exit 99
 fi
