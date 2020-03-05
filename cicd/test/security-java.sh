9dbc10665467ffe6b216a3d168d432b397937c26
Glen Hickman <gchickma@us.ibm.com>
2020-03-05 15:03:01 -0600

    add asoc debugging

--- cicd/test/security-java.sh
+++ cicd/test/security-java.sh
@@ -11,10 +11,12 @@ ASOC_APP_ID=$6
 ASOC_LOGIN_KEY_ID=$7
 ASOC_LOGIN_SECRET=$8
 
+echo "No Proxy in ASoC: $NO_PROXY"
+
 # Download ASoC CLI
 ART_REPO_ZIP_FOLDER=asoc
 ART_REPO_ZIP_FILE=SAClientUtil_7.0.1313_linux.zip
-curl --noproxy "*" --insecure -u $ART_REPO_USER:$ART_REPO_PASSWORD "$ART_URL/$ART_REPO_ZIP_FOLDER/$ART_REPO_ZIP_FILE" -o SAClientUtil.zip
+curl --noproxy $NO_PROXY --insecure --max-time 120 -u $ART_REPO_USER:$ART_REPO_PASSWORD "$ART_URL/$ART_REPO_ZIP_FOLDER/$ART_REPO_ZIP_FILE" -o SAClientUtil.zip
 
 # Unzip ASoC CLI
 unzip SAClientUtil.zip
