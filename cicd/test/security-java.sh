f9632657bfdb840c7c59c1a785bc439125e48275
Glen Hickman <gchickma@us.ibm.com>
2020-03-06 15:57:23 -0600

    move appscan cli to parent folder

--- cicd/test/security-java.sh
+++ cicd/test/security-java.sh
@@ -23,6 +23,7 @@ unzip SAClientUtil.zip
 rm -f SAClientUtil.zip
 SAC_DIR=`ls -d SAClientUtil*`
 mv $SAC_DIR SAClientUtil
+mv SAClientUtil ../SAClientUtil
 
 # Check JAVA_HOME is set
 echo "JAVA_HOME=$JAVA_HOME"
@@ -34,7 +35,7 @@ if [ "$HTTP_PROXY" != "" ]; then
     export MAVEN_OPTS="-Dhttp.proxyHost=$PROXY_HOST -Dhttp.proxyPort=$PROXY_PORT -Dhttp.nonProxyHosts='$MAVEN_PROXY_IGNORE' -Dhttps.proxyHost=$PROXY_HOST -Dhttps.proxyPort=$PROXY_PORT -Dhttps.nonProxyHosts='$MAVEN_PROXY_IGNORE'"
 fi
 echo "MAVEN_OPTS=$MAVEN_OPTS"
-mvn compile dependency:copy-dependencies $MAVEN_OPTS
+mvn install $MAVEN_OPTS
 
 # Create appscan-config.xml
 cat >> glen-appscan-config.xml <<EOL
@@ -43,6 +44,7 @@ cat >> glen-appscan-config.xml <<EOL
    <Targets>
     <Target path="target/classes">
       <CustomBuildInfo additional_classpath="target/dependency;target/classes" src_root="src/main/java;src/test/java;src/main/resources" jdk_path="$JAVA_HOME" />
+      <Exclude>SAClientUtil/</Exclude>
     </Target>
   </Targets>
 </Configuration>
@@ -52,12 +54,12 @@ EOL
 export APPSCAN_OPTS="-Dhttp.proxyHost=$PROXY_HOST -Dhttp.proxyPort=$PROXY_PORT -Dhttps.proxyHost=$PROXY_HOST -Dhttps.proxyPort=$PROXY_PORT"
 echo "APPSCAN_OPTS=$APPSCAN_OPTS"
 #SAClientUtil/bin/appscan.sh prepare -c appscan-config.xml -n ${COMPONENT_NAME}_${VERSION_NAME}.irx
-SAClientUtil/bin/appscan.sh prepare
+SAClientUtil/bin/appscan.sh prepare -v -X -n ${COMPONENT_NAME}_${VERSION_NAME}.irx
 
 ls -al
 
 echo "========================================================================================="
-cat appscan-config.xml
+#cat appscan-config.xml
 echo "========================================================================================="
 
 cat SAClientUtil/logs/client.log
@@ -73,8 +75,8 @@ echo "ASoC App ID: $ASOC_APP_ID"
 echo "ASoC Login Key ID: $ASOC_LOGIN_KEY_ID"
 echo "ASoC Login Secret ID: $ASOC_LOGIN_SECRET"
 
-SAClientUtil/bin/appscan.sh api_login -u $ASOC_LOGIN_KEY_ID -P $ASOC_LOGIN_SECRET
-ASOC_SCAN_ID=$(SAClientUtil/bin/appscan.sh queue_analysis -a $ASOC_APP_ID -f ${COMPONENT_NAME}_${VERSION_NAME}.irx -n ${COMPONENT_NAME}_${VERSION_NAME} | tail -n 1)
+../SAClientUtil/bin/appscan.sh api_login -u $ASOC_LOGIN_KEY_ID -P $ASOC_LOGIN_SECRET
+ASOC_SCAN_ID=$(../SAClientUtil/bin/appscan.sh queue_analysis -a $ASOC_APP_ID -f ${COMPONENT_NAME}_${VERSION_NAME}.irx -n ${COMPONENT_NAME}_${VERSION_NAME} | tail -n 1)
 echo "ASoC Scan ID: $ASOC_SCAN_ID"
 
 if [ -z "$ASOC_SCAN_ID" ]; then
@@ -83,7 +85,7 @@ fi
 
 START_SCAN=`date +%s`
 RUN_SCAN=true
-while [ "$(SAClientUtil/bin/appscan.sh status -i $ASOC_SCAN_ID)" != "Ready" ] && [ "$RUN_SCAN" == "true" ]; do
+while [ "$(../SAClientUtil/bin/appscan.sh status -i $ASOC_SCAN_ID)" != "Ready" ] && [ "$RUN_SCAN" == "true" ]; do
   NOW=`date +%s`
   DIFF=`expr $NOW - $START_SCAN`
   if [ $DIFF -gt 300 ]; then
@@ -100,10 +102,10 @@ if [ "$RUN_SCAN" == "false" ]; then
 fi
 
 #Get ASoC execution summary
-SAClientUtil/bin/appscan.sh info -i $ASOC_SCAN_ID -json >> ASOC_Summary.json
+../SAClientUtil/bin/appscan.sh info -i $ASOC_SCAN_ID -json >> ASOC_Summary.json
 
 # Download ASoC report
-SAClientUtil/bin/appscan.sh get_result -d ASOC_SCAN_RESULTS_${COMPONENT_NAME}_${VERSION_NAME}.html -i $ASOC_SCAN_ID
+../SAClientUtil/bin/appscan.sh get_result -d ASOC_SCAN_RESULTS_${COMPONENT_NAME}_${VERSION_NAME}.html -i $ASOC_SCAN_ID
 
 cat ASOC_SCAN_RESULTS_${COMPONENT_NAME}_${VERSION_NAME}.html
 
