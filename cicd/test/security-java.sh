5c75e7ef448bb4c156710e5c3343a3ee5531dd5e
Glen Hickman <gchickma@us.ibm.com>
2020-03-11 12:05:47 -0500

    link jre under cli to openjdk

--- cicd/test/security-java.sh
+++ cicd/test/security-java.sh
@@ -92,6 +92,11 @@ $JAVA_HOME/jre/bin/java -version
 
 export JAVACMD="$JAVA_HOME/jre/bin/java"
 echo "JAVACMD=$JAVACMD"
+$JAVACMD
+
+rm /data/SAClientUtil/jre/bin/java
+ln -s /usr/lib/jvm/java-1.8-openjdk/jre/bin/java /data/SAClientUtil/jre/bin/java
+/data/SAClientUtil/jre/bin/java -version
 
 # Generate IRX file
 export APPSCAN_OPTS="-Dhttp.proxyHost=$PROXY_HOST -Dhttp.proxyPort=$PROXY_PORT -Dhttps.proxyHost=$PROXY_HOST -Dhttps.proxyPort=$PROXY_PORT"
