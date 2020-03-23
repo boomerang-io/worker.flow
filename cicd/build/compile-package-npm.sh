be277b2c17c3c1b006ca8d3b3c82bd071db564c1
Timothy R. Bula <trbula@us.ibm.com>
2020-03-23 16:31:09 -0400

    feat: publish npm packages

--- cicd/build/compile-package-npm.sh
+++ cicd/build/compile-package-npm.sh
@@ -19,13 +19,13 @@ else
     exit 99
 fi
 
-SCRIPT=$(node -pe "require('./package.json').scripts.build");
+SCRIPT=$(node -pe "require('./package.json').scripts.publish");
 if [ "$SCRIPT" != "undefined" ]
 then
     if [ "$BUILD_TOOL" == "npm" ]; then
-        npm run build
+        npm publish
     elif [ "$BUILD_TOOL" == "yarn" ]; then
-        yarn run build
+        yarn publish
     else
         exit 97
     fi
