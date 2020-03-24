7c7fd39c6e795eca15481cd9aa28b7bb5a52c5a4
Glen Hickman <gchickma@us.ibm.com>
2020-03-24 13:48:19 -0500

    revert to alpine:3.9

--- cicd/test/initialize-dependencies-unit-java.sh
+++ cicd/test/initialize-dependencies-unit-java.sh
@@ -8,12 +8,12 @@ export LANGUAGE=en_US.UTF-8
 apk --no-cache add ca-certificates
 
 wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
-wget -q -O glibc-2.31-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.31-r0/glibc-2.31-r0.apk
-wget -q -O glibc-bin-2.31-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.31-r0/glibc-bin-2.31-r0.apk
-# wget -q -O glibc-dev-2.31-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.31-r0/glibc-dev-2.31-r0.apk
-wget -q -O glibc-i18n-2.31-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.31-r0/glibc-i18n-2.31-r0.apk
+wget -q -O glibc-2.30-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.30-r0/glibc-2.30-r0.apk
+wget -q -O glibc-bin-2.30-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.30-r0/glibc-bin-2.30-r0.apk
+# wget -q -O glibc-dev-2.30-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.30-r0/glibc-dev-2.30-r0.apk
+wget -q -O glibc-i18n-2.30-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.30-r0/glibc-i18n-2.30-r0.apk
 
-apk add glibc-2.31-r0.apk glibc-bin-2.31-r0.apk glibc-i18n-2.31-r0.apk
+apk add glibc-2.30-r0.apk glibc-bin-2.30-r0.apk glibc-i18n-2.30-r0.apk
 
 /usr/glibc-compat/bin/localedef -i en_US -f UTF-8 en_US.UTF-8
 
