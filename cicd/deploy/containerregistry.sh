35d6fad920860e5b832f4890d672fa7f010c3869
Tyson Lawrie <twlawrie@us.ibm.com>
2020-03-31 13:52:30 +1100

    :bug: PLT-4003 updated container registry publishing

--- cicd/deploy/containerregistry.sh
+++ cicd/deploy/containerregistry.sh
@@ -20,44 +20,52 @@ GLOBAL_REGISTRY_PASSWORD=${12}
 
 IMG_OPTS=
 SKOPEO_OPTS=
-# if [ "$DEBUG" == "true" ]; then
+if [ "$DEBUG" == "true" ]; then
     echo "Enabling debug logging..."
     IMG_OPTS+="-d"
     SKOPEO_OPTS+="--debug "
-# fi
+fi
 
-# Log into the platforms global container registry
+# Set source connection settings
 if [ "$GLOBAL_REGISTRY_PORT" != "undefined" ]; then
     GLOBAL_DOCKER_SERVER="$GLOBAL_REGISTRY_HOST:$GLOBAL_REGISTRY_PORT"
 else 
     GLOBAL_DOCKER_SERVER="$GLOBAL_REGISTRY_HOST"
 fi
-echo "Logging into Boomerang Container Registry ($GLOBAL_DOCKER_SERVER)..."
-/opt/bin/img login $IMG_OPTS -u=$GLOBAL_REGISTRY_USER -p=$GLOBAL_REGISTRY_PASSWORD "$GLOBAL_DOCKER_SERVER"
 
-# Login to the destination
-echo "DESTINATION_REGISTRY_PORT=$DESTINATION_REGISTRY_PORT"
+# Set source credentials
+if [ ! -z "$GLOBAL_REGISTRY_USER" ] || [ ! -z "$GLOBAL_REGISTRY_PASSWORD" ]; then
+    echo "Configuring origin container registry credentials..."
+    GLOBAL_REGISTRY_CREDS="--src-creds $GLOBAL_REGISTRY_USER:$GLOBAL_REGISTRY_PASSWORD"
+else
+    echo "Skipping origin container registry credentials..."
+    GLOBAL_REGISTRY_CREDS="--src-no-creds=true"
+fi
+echo "GLOBAL_REGISTRY_CREDS: $GLOBAL_REGISTRY_CREDS"
+
+# Set Destination Connection Settings"
 if [ "$DESTINATION_REGISTRY_PORT" != "undefined" ] && [ ! -z "$DESTINATION_REGISTRY_PORT" ] && [[ ! $DESTINATION_REGISTRY_HOST =~ "icr.io" ]]; then
     DESTINATION_DOCKER_SERVER="$DESTINATION_REGISTRY_HOST:$DESTINATION_REGISTRY_PORT"
 else
     DESTINATION_DOCKER_SERVER="$DESTINATION_REGISTRY_HOST"
 fi
-if [ ! -z "$DESTINATION_REGISTRY_USER" ] || [ ! -z "$DESTINATION_REGISTRY_PASSWORD" ]; then
-    echo "Logging into destinations container registry ($DESTINATION_DOCKER_SERVER)..."
-    /opt/bin/img login $IMG_OPTS -u=$DESTINATION_REGISTRY_USER -p="$DESTINATION_REGISTRY_PASSWORD" "$DESTINATION_DOCKER_SERVER"
-else
-    echo "Skipping destination registry login as no username and / or password provided. "
-fi
 
-echo "Origin: $GLOBAL_DOCKER_SERVER/$IMAGE_ORG/$IMAGE_NAME:$VERSION_NAME"
-echo "Destination: $DESTINATION_DOCKER_SERVER$DESTINATION_REGISTRY_IMAGE_PATH/$IMAGE_NAME:$VERSION_NAME"
-if [[ $DESTINATION_REGISTRY_HOST =~ "icr.io" ]]; then
-    skopeo $SKOPEO_OPTS copy --dest-tls-verify=false  --dest-creds $DESTINATION_REGISTRY_USER:$DESTINATION_REGISTRY_PASSWORD docker://$DESTINATION_DOCKER_SERVER/$IMAGE_ORG/$IMAGE_NAME:$VERSION_NAME docker://$DESTINATION_DOCKER_SERVER$DESTINATION_REGISTRY_IMAGE_PATH/$IMAGE_NAME:$VERSION_NAME
-    RESULT=$?
+# Set Destination Credentials
+if [ ! -z "$DESTINATION_REGISTRY_USER" ] || [ ! -z "$DESTINATION_REGISTRY_PASSWORD" ]; then
+    echo "Configuring destination container registry credentials..."
+    DESTINATION_REGISTRY_CREDS="--dest-creds $DESTINATION_REGISTRY_USER:$DESTINATION_REGISTRY_PASSWORD"
 else
-    skopeo $SKOPEO_OPTS copy --dest-tls-verify=false docker://$DESTINATION_DOCKER_SERVER/$IMAGE_ORG/$IMAGE_NAME:$VERSION_NAME docker://$DESTINATION_DOCKER_SERVER$DESTINATION_REGISTRY_IMAGE_PATH/$IMAGE_NAME:$VERSION_NAME
-    RESULT=$?
+    echo "Skipping destination container registry credentials..."
+    DESTINATION_REGISTRY_CREDS="--dest-no-creds=true"
 fi
+echo "DESTINATION_REGISTRY_CREDS: $DESTINATION_REGISTRY_CREDS"
+
+echo "Copying from Origin to Destination..."
+echo "- Origin: $GLOBAL_DOCKER_SERVER/$IMAGE_ORG/$IMAGE_NAME:$VERSION_NAME"
+echo "- Destination: $DESTINATION_DOCKER_SERVER$DESTINATION_REGISTRY_IMAGE_PATH/$IMAGE_NAME:$VERSION_NAME"
+echo ""
+skopeo --insecure-policy $SKOPEO_OPTS copy --src-tls-verify=false --dest-tls-verify=false $GLOBAL_REGISTRY_CREDS $DESTINATION_REGISTRY_CREDS docker://$GLOBAL_DOCKER_SERVER/$IMAGE_ORG/$IMAGE_NAME:$VERSION_NAME docker://$DESTINATION_DOCKER_SERVER$DESTINATION_REGISTRY_IMAGE_PATH/$IMAGE_NAME:$VERSION_NAME
+RESULT=$?
 if [ $RESULT -ne 0 ] ; then
     exit 88
 fi
