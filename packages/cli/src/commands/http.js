834cbb0574cf264a75f3fa00e5213dd0ad6de368
Timothy R. Bula <trbula@us.ibm.com>
2020-04-13 11:49:28 -0400

    feat: first commit with two packages

---
+++ packages/cli/src/commands/http.js
@@ -0,0 +1,73 @@
+const { log, utils } = require("@boomerang-worker/core");
+const HttpsProxyAgent = require("https-proxy-agent");
+const https = require("https");
+const URL = require("url");
+
+module.exports = {
+  /**
+   * @todo implement a fetch that takes in;
+   * URL [String]
+   * method [Select - Options: get, post, put, patch, delete, options]
+   * headers [Text Area - new line delimitered list?]
+   * content type [Select - Options: any, text, xml, json, html]
+   * body [Text Area - optional depending on method]
+   * Allow untrusted SSL certs [Boolean Toggle]
+   */
+
+  execute() {
+    log.debug("Started HTTP Call Plugin");
+
+    //Destructure and get properties ready.
+    const taskProps = utils.substituteTaskInputPropsValuesForWorkflowInputProps();
+    const { url, method, header, contentType, body, allowUntrustedCerts } = taskProps;
+    const headerObject = JSON.parse(header);
+    const bodyStringfy = JSON.stringify(body);
+    /** @todo finish out passing in of parameters*/
+
+    var agent = null;
+    if (process.env.HTTP_PROXY) {
+      log.debug("Using Proxy", process.env.HTTP_PROXY);
+      agent = new HttpsProxyAgent(process.env.HTTP_PROXY);
+    }
+
+    if (allowUntrustedCerts) {
+      log.sys(`Attempting HTTP request allowing untrusted certs`);
+    }
+
+    const opts = URL.parse(url);
+    opts.rejectUnauthorized = !allowUntrustedCerts;
+    opts.agent = agent;
+    opts.method = method;
+    opts.header = {
+      ...headerObject,
+      "Content-Type": contentType
+    };
+    const req = https.request(opts, res => {
+      log.sys(`statusCode: ${res.statusCode}`);
+      let output = "";
+
+      res.on("data", d => {
+        output += d;
+      });
+
+      res.on("end", () => {
+        const response = JSON.parse(output);
+        log.sys("Response Received:", JSON.stringify(response));
+        utils.setOutputProperty("response", JSON.stringify(response));
+        log.good("Response successfully received!");
+      });
+    });
+
+    req.on("error", err => {
+      log.err(err);
+      process.exit(1);
+    });
+
+    if (bodyStringfy && bodyStringfy !== "") {
+      req.write(bodyStringfy);
+    }
+
+    req.end();
+    log.debug("Finished HTTP Call File Plugin");
+  }
+};
---
+++ packages/cli/src/commands/notifications.js
@@ -0,0 +1,50 @@
+const { log, utils } = require("@boomerang-worker/core");
+const fetch = require("node-fetch");
+
+module.exports = {
+  sendNotification() {
+    log.debug("Starting Platform NotificationPlugin");
+
+    const taskProps = utils.substituteTaskInputPropsValuesForWorkflowInputProps();
+    const { type, target, title, message } = taskProps;
+
+    if (type === undefined || type === null) {
+      log.err("No type has been specified");
+      process.exit(1);
+    }
+
+    bodyString = JSON.stringify({
+      target: {
+        type: type,
+        userId: type === "user" ? target : "",
+        groupName: type === "group" ? target : ""
+      },
+      payload: {
+        title: title,
+        content: message,
+        type: "notification"
+      }
+    });
+    try {
+      fetch("http://bmrg-core-services-notifications.bmrg-live/notifications/submit", {
+        method: "POST",
+        body: bodyString,
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": "96f0b5a2-2e23-4561-a877-005c24df9805"
+        }
+      })
+        .then(res => res.json())
+        .then(json => log.debug(json))
+        .catch(err => {
+          log.err(err);
+          process.exit(1);
+        });
+    } catch (e) {
+      log.err(e);
+      process.exit(1);
+    }
+
+    log.debug("Finished Platform Notification Plugin");
+  }
+};
