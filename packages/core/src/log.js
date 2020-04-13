0c38c9416702638389ea28ed114383e34ba7dc32
Timothy R. Bula <trbula@us.ibm.com>
2020-04-13 12:29:04 -0400

    feat: add linting and fix some errors

---
+++ packages/core/src/log.js
@@ -0,0 +1,31 @@
+const isDebug = process.env.DEBUG === "true";
+const chalk = require("chalk");
+const datetime = require("node-datetime");
+
+module.exports = {
+  out(...args) {
+    console.log(chalk.white("  ", args[1]));
+  },
+  debug(...args) {
+    if (isDebug) {
+      console.log(chalk.gray(`${datetime.create().format("m/d/y H:M:S")}`), "🔍 ", ...args);
+    }
+  },
+  sys(...args) {
+    console.log(chalk.blue(`${datetime.create().format("m/d/y H:M:S")}`, "🤖 ", ...args));
+  },
+  ci(...args) {
+    console.log(
+      chalk.blue(`${datetime.create().format("m/d/y H:M:S")}`, "🏗️ ", "-".repeat(20), ...args, "-".repeat(20))
+    );
+  },
+  good(...args) {
+    console.log(chalk.green(`${datetime.create().format("m/d/y H:M:S")}`, "✅ ", ...args));
+  },
+  warn(...args) {
+    console.log(chalk.yellow(`${datetime.create().format("m/d/y H:M:S")}`, "⚠️ ", ...args));
+  },
+  err(...args) {
+    console.log(chalk.red(`${datetime.create().format("m/d/y H:M:S")}`, "❗ ", ...args));
+  },
+};
