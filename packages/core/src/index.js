66230e23a3d073197c45292928e0bb6c62fc2033
Timothy R. Bula <trbula@us.ibm.com>
2020-04-14 19:39:06 -0400

    feat: add common

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,9 +1,11 @@
 const CICDError = require("./error");
 const log = require("./log");
 const utils = require("./utils");
+const common = require("./common");
 
 module.exports = {
   CICDError,
+  common,
   log,
   utils,
 };
