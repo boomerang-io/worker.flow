d0f415711196b2fef00d2af9679c89c14c34eb9e
Tyson Lawrie <twlawrie@us.ibm.com>
2020-07-01 18:54:13 +1000

    feat: :bug: work around for twilio request client fix

--- commands/twilio.js
+++ commands/twilio.js
@@ -1,6 +1,6 @@
 const { log, utils } = require("@boomerang-worker/core");
 const Twilio = require("twilio");
-const TwilioRequestClient = require("twilio/lib/base/RequestClient.js");
+// const TwilioRequestClient = require("twilio/lib/base/RequestClient.js");
 
 module.exports = {
   async sendSMS() {
@@ -11,32 +11,20 @@ module.exports = {
     const taskProps = utils.substituteTaskInputPropsValuesForWorkflowInputProps();
     const { accountSid, token, from, to, message } = taskProps;
 
-    // log.debug("To", to);
-    // log.debug("From", from);
-
-    // if (!String(to).startsWith("+") || !String(from).startsWith("+")) {
-    //     // Logic taken from https://www.twilio.com/docs/sms/tutorials/how-to-send-sms-messages-node-js
-    //     log.err("Both the from and to parameters must use E.164 formatting ('+' and a country code, e.g., '+1')");
-    //     process.exit(1);
-    // }
-
     // Do we want a foreach multiline supported?
     // https://www.twilio.com/docs/sms/tutorials/how-to-send-sms-messages-node-js#send-a-message-to-multiple-recipients
     // toArray = to !== null ? to.split("\n") : [];
 
-    log.debug("HTTP_PROXY:", process.env.HTTP_PROXY);
-    const client = Twilio(accountSid, token, {
-      httpClient: new TwilioRequestClient(process.env.HTTP_PROXY)
-    });
+    const client = Twilio(accountSid, token);
 
     try {
-      client.messages
+      await client.messages
         .create({
           body: message,
           from: from,
           to: to
         })
-        .then(message => log.good(message));
+        .then(message => log.good(JSON.stringify(message)));
     } catch (error) {
       log.err(error);
       process.exit(1);
