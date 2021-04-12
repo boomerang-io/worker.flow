2158665482c9bca9da8d56598b9d932ca1a30ee2
Costel Moraru <moraru_costel@yahoo.com>
2021-04-12 16:24:53 +0200

    Fixing the output param

--- commands/slack.js
+++ commands/slack.js
@@ -679,7 +679,8 @@ module.exports = {
       .then(body => {
         log.sys("Response Received:", JSON.stringify(body));
         log.sys("Setting output param:", getCloudEvent("slack-conversations-members", "SlackMembers", body));
-        utils.setOutputParameter("output", JSON.stringify(getCloudEvent("slack-conversations-members", "SlackMembers", body)));
+        //utils.setOutputParameter("output", JSON.stringify(getCloudEvent("slack-conversations-members", "SlackMembers", body)));
+        utils.setOutputParameter("output", JSON.stringify(body));
         log.good("Response successfully received!");
       })
       .catch(err => {
