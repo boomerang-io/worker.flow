cd1e6ec5ac91c29e419cf4cc4f8f9576d98a8526
Costel Moraru <moraru_costel@yahoo.com>
2021-06-15 22:09:08 +0200

    Adding a check if collaborator exist before inviting him/her

--- commands/github.js
+++ commands/github.js
@@ -859,13 +859,31 @@ module.exports = {
         repo: repoURLAtomsArray[repoURLAtomsArray.length - 1],
         username: users[0].login
       };
-      if (protectAgainstEmpty(permission)) {
-        data["permission"] = permission;
-      }
-      await octokit.repos.addCollaborator(data).then(body => {
-        log.debug("Response Received:", body);
-        log.good("Response successfully received!");
+      let collaboratorExists = false;
+      octokit.hook.after("request", async response => {
+        log.debug("response.status : ", response.status);
+        if (response.status === 204) {
+          collaboratorExists = true;
+        }
       });
+      octokit.hook.error("request", async error => {
+        if (error.status === 404) {
+          collaboratorExists = false;
+        }
+      });
+      await octokit.repos.checkCollaborator(data).then(body => {
+        log.debug("Check Collaborator Response Received:", body);
+      });
+      log.good("Collaborator already exists: ", collaboratorExists);
+      if (!collaboratorExists) {
+        if (protectAgainstEmpty(permission)) {
+          data["permission"] = permission;
+        }
+        await octokit.repos.addCollaborator(data).then(body => {
+          log.debug("Response Received:", body);
+          log.good("Response successfully received!");
+        });
+      }
     } catch (error) {
       log.err(error);
       process.exit(1);
