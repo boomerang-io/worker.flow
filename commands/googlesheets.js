b597052117418c5e82613fbf50716bced150787f
George Safta <saftageorge@gmail.com>
2021-10-25 19:32:11 +0300

    add google sheets commands

--- commands/googlesheets.js
+++ commands/googlesheets.js
@@ -6,6 +6,7 @@ const CREDENTIAL_TYPE = "service_account";
 const AUTH_URI = "https://accounts.google.com/o/oauth2/auth";
 const TOKEN_URI = "https://oauth2.googleapis.com/token";
 const AUTH_PROVIDER_CERT_URL = "https://www.googleapis.com/oauth2/v1/certs";
+const X_509_CERT_URL = "https://www.googleapis.com/robot/v1/metadata/x509/";
 
 //Internal helper function
 function assertExists(value, message) {
@@ -24,7 +25,7 @@ function getCredentials(taskProps) {
   assertExists(clientEmail, "Client email must be provided");
   assertExists(clientId, "Client Id must be provided");
 
-  const certURL = "https://www.googleapis.com/robot/v1/metadata/x509/" + encodeURIComponent(clientEmail);
+  const certURL = X_509_CERT_URL + encodeURIComponent(clientEmail);
 
   const creds = {
     type: CREDENTIAL_TYPE,
@@ -49,24 +50,100 @@ function getClient(creds) {
   return client;
 }
 
-async function getSheets() {
+async function getData() {
+  log.debug("Starting getData from Google Sheets task");
+
   const taskProps = utils.resolveInputParameters();
   const creds = getCredentials(taskProps);
   const sheets = google.sheets({ version: "v4", auth: getClient(creds) });
-  const { spreadsheetId } = taskProps;
+  const { spreadsheetId, range } = taskProps;
   assertExists(spreadsheetId, "Spreadsheet Id must be provided");
-  const response = await sheets.spreadsheets.get({
-    spreadsheetId
-  });
-  log.good(JSON.stringify(response.data));
+  assertExists(range, "Range must be provided");
 
-  const getRows = await sheets.spreadsheets.values.get({
-    spreadsheetId,
-    range: "Sheet1"
-  });
-  log.good(JSON.stringify(getRows.data));
+  await sheets.spreadsheets.values
+    .get({
+      spreadsheetId,
+      range
+    })
+    .then(body => {
+      const values = JSON.stringify(body.data.values);
+      log.sys("Response Received:", values);
+      utils.setOutputParameter("rows", values);
+      log.good("Response successfully received!");
+    })
+    .catch(err => {
+      log.err(err);
+      process.exit(1);
+    });
+
+  log.debug("Finished getData Google Sheets task");
+}
+
+async function batchGetData() {
+  log.debug("Starting batchGetData from Google Sheets task");
+
+  const taskProps = utils.resolveInputParameters();
+  const creds = getCredentials(taskProps);
+  const sheets = google.sheets({ version: "v4", auth: getClient(creds) });
+  const { spreadsheetId, ranges } = taskProps;
+  assertExists(spreadsheetId, "Spreadsheet Id must be provided");
+  assertExists(ranges, "Range must be provided");
+
+  await sheets.spreadsheets.values
+    .batchGet({
+      spreadsheetId,
+      ranges: ranges.split(",")
+    })
+    .then(body => {
+      const values = JSON.stringify(body.data.valueRanges.map(r => r.values).reduce((a, b) => a.concat(b), []));
+      log.sys("Response Received:", values);
+      utils.setOutputParameter("rows", values);
+      log.good("Response successfully received!");
+    })
+    .catch(err => {
+      log.err(err);
+      process.exit(1);
+    });
+
+  log.debug("Finished batchGetData Google Sheets task");
+}
+
+async function append() {
+  log.debug("Starting append data to Google Sheets task");
+
+  const taskProps = utils.resolveInputParameters();
+  const creds = getCredentials(taskProps);
+  const sheets = google.sheets({ version: "v4", auth: getClient(creds) });
+  const { spreadsheetId, range, values } = taskProps;
+  assertExists(spreadsheetId, "Spreadsheet Id must be provided");
+  assertExists(range, "Range must be provided");
+  assertExists(values, "Values must be provided");
+
+  await sheets.spreadsheets.values
+    .append({
+      spreadsheetId,
+      range,
+      valueInputOption: "USER_ENTERED",
+      requestBody: {
+        values
+      }
+    })
+    .then(body => {
+      const response = JSON.stringify(body);
+      log.sys("Response Received:", response);
+      utils.setOutputParameter("response", response);
+      log.good("Response successfully received!");
+    })
+    .catch(err => {
+      log.err(err);
+      process.exit(1);
+    });
+
+  log.debug("Finished append data to Google Sheets task");
 }
 
 module.exports = {
-  getSheets
+  append,
+  batchGetData,
+  getData
 };
